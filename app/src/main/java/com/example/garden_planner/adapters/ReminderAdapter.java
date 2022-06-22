ac4a17b9fdcc7701f40517d69eb090340cf0c972
Sarah Motteler <sarahmotteler@fb.com>
2022-06-22 16:48:34 -0700

    added code to ReminderAdapter

--- app/src/main/java/com/example/garden_planner/adapters/ReminderAdapter.java
+++ app/src/main/java/com/example/garden_planner/adapters/ReminderAdapter.java
@@ -1,4 +1,91 @@
 package com.example.garden_planner.adapters;
 
-public class ReminderAdapter {
-}
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.garden_planner.databinding.FragmentRemindersBinding;
+import com.example.garden_planner.databinding.ItemReminderBinding;
+import com.example.garden_planner.models.Reminder;
+
+import java.util.List;
+
+public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.ViewHolder> {
+
+    public static final String TAG = "ReminderAdapter";
+    private Context context;
+    private List<Reminder> reminders;
+
+    ItemReminderBinding binding;
+
+    public ReminderAdapter(Context context, List<Reminder> reminders){
+        Log.i(TAG, "making ReminderAdapter");
+        this.context = context;
+        this.reminders = reminders;
+    }
+
+    @NonNull
+    @Override
+    public ReminderAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.i(TAG, "onCreateViewHolder ");
+
+        binding = ItemReminderBinding.inflate( LayoutInflater.from(context), parent, false);
+        View view = binding.getRoot();
+
+        return  new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReminderAdapter.ViewHolder holder, int position) {
+        Log.i(TAG, "onBindViewHolder " + position);
+        Reminder reminder = reminders.get(position);
+        holder.bind(reminder);
+    }
+
+    @Override
+    public int getItemCount() {
+        return reminders.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private TextView tvGardenName;
+        private TextView tvToDoDate;
+        private ImageView ivPlantPic;
+        private TextView tvPlantName;
+        private TextView tvReminderTitle;
+        private TextView tvReminderText;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION) {
+                Reminder reminder = reminders.get(position);
+                // TODO: open up the garden that the reminder pertains to
+            }
+        }
+
+        public void bind(Reminder reminder){
+            tvGardenName = binding.tvGardenName;
+            tvToDoDate = binding.tvToDoDate;
+            ivPlantPic = binding.ivPlantPic;
+            tvPlantName = binding.tvPlantName;
+            tvReminderTitle = binding.tvReminderTitle;
+            tvReminderText = binding.tvReminderText;
+
+        }
+
+    }
+    }

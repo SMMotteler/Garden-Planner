2070ff561b275da66268ff8b871b11b9bf43ea3a
Sarah Motteler <sarahmotteler@fb.com>
2022-06-22 14:09:47 -0700

    added Reminder getter and setter methods

--- app/src/main/java/com/example/garden_planner/models/Reminder.java
+++ app/src/main/java/com/example/garden_planner/models/Reminder.java
@@ -2,7 +2,35 @@ package com.example.garden_planner.models;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.util.Date;
 
 @ParseClassName("Reminder")
 public class Reminder extends ParseObject {
+    public static final String KEY_REMINDER_START = "ReminderStart";
+    public static final String KEY_REMINDER_END = "ReminderEnd";
+    public static final String KEY_REMINDER_MESSAGE = "ReminderMessage";
+    public static final String KEY_REMIND_WHAT = "RemindWhat";
+    public static final String KEY_REMIND_WHO = "RemindWho";
+
+    public Date getReminderStart(){return getDate(KEY_REMINDER_START);}
+
+    public void setReminderStart(Date reminderStart){put(KEY_REMINDER_START, reminderStart);}
+
+    public Date getReminderEnd(){return getDate(KEY_REMINDER_END);}
+
+    public void setReminderEnd(Date reminderEnd){put(KEY_REMINDER_END, reminderEnd);}
+
+    public String getReminderMessage(){return getString(KEY_REMINDER_MESSAGE);}
+
+    public void setReminderMessage(String reminderMessage){put(KEY_REMINDER_MESSAGE, reminderMessage);}
+
+    public Garden getRemindWhat(){return (Garden)get(KEY_REMIND_WHAT);}
+
+    public void setRemindWhat(Garden remindWhat){put(KEY_REMIND_WHAT, remindWhat);}
+
+    public ParseUser getRemindWho(){return getParseUser(KEY_REMIND_WHO);}
+
+    public void setRemindWho(ParseUser remindWho){put(KEY_REMIND_WHO, remindWho);}
 }

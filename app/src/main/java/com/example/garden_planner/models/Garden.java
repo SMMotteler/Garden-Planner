f4332cb7f9a7de692ea01a78f506c9924e5d2cf2
Sarah Motteler <sarahmotteler@fb.com>
2022-06-22 11:39:50 -0700

    added getter and setter methods for Garden and Plant objects

--- app/src/main/java/com/example/garden_planner/models/Garden.java
+++ app/src/main/java/com/example/garden_planner/models/Garden.java
@@ -3,6 +3,34 @@ package com.example.garden_planner.models;
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
 
+import java.util.List;
+
 @ParseClassName("Garden")
 public class Garden extends ParseObject {
+    public static final String KEY_NAME = "name";
+    public static final String KEY_LATITUDE = "Latitude";
+    public static final String KEY_LONGITUDE = "Longitude";
+    public static final String KEY_PLANTS = "plants";
+
+    public String getName(){return getString(KEY_NAME);}
+
+    public void setName(String name){put(KEY_NAME, name);}
+
+    public Long getLatitude(){return getLong(KEY_LATITUDE);}
+
+    public void setLatitude(Long latitude){put(KEY_LATITUDE, latitude);}
+
+    public Long getLongitude(){return getLong(KEY_LONGITUDE);}
+
+    public void setLongitude(Long longitude){put(KEY_LONGITUDE, longitude);}
+
+    public List<Plant> getPlants(){return getList(KEY_PLANTS);}
+
+    public void setPlants(List<Plant> plants){ put(KEY_PLANTS, plants);}
+
+    public void addPlant(Plant plant){
+        List<Plant> currentPlants = getPlants();
+        currentPlants.add(plant);
+        setPlants(currentPlants);
+    }
 }

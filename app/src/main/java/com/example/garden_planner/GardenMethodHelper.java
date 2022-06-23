c147e27c211eb9f50399f5895d8fcc7b9fcb06a8
Sarah Motteler <sarahmotteler@fb.com>
2022-06-23 15:14:22 -0700

    created queryGarden method and updated garden class

--- app/src/main/java/com/example/garden_planner/GardenMethodHelper.java
+++ app/src/main/java/com/example/garden_planner/GardenMethodHelper.java
@@ -2,12 +2,19 @@ package com.example.garden_planner;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.example.garden_planner.adapters.GardenFeedAdapter;
+import com.example.garden_planner.models.Garden;
+import com.parse.FindCallback;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class GardenMethodHelper {
 
@@ -37,6 +44,37 @@ public class GardenMethodHelper {
             return "android.resource://com.example.parsetagram/"+R.drawable.default_pic;
         }
         return image.getUrl();
+    }
+
+    public void queryGarden(List<Garden> userGardens, GardenFeedAdapter adapter, ParseUser user){
+        ParseQuery<Garden> query = ParseQuery.getQuery(Garden.class);
+
+        query.whereEqualTo(Garden.KEY_USER, user);
+        query.addDescendingOrder("createdAt");
+        query.include(Garden.KEY_NAME);
+        // start an asynchronous call for comments
+        query.findInBackground(new FindCallback<Garden>() {
+            @Override
+            public void done(List<Garden> gardens, ParseException e) {
+                // check for errors
+                if (e != null) {
+                    Log.e("Detail Activity", "Issue with getting gardens", e);
+                    return;
+                }
+
+                // for debugging purposes let's print every garden name to LogCat
+                for (Garden garden : gardens) {
+                    Log.i("Garden Query", "Garden: " + garden.getName());
+                }
+
+                // save user garden to list and notify adapter of new data
+                userGardens.clear();
+                userGardens.addAll(gardens);
+
+                adapter.notifyDataSetChanged();
+            }
+        });
+
     }
 
 }
--- app/src/main/java/com/example/garden_planner/models/Garden.java
+++ app/src/main/java/com/example/garden_planner/models/Garden.java
@@ -1,7 +1,9 @@
 package com.example.garden_planner.models;
 
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 import java.util.List;
 
@@ -11,6 +13,9 @@ public class Garden extends ParseObject {
     public static final String KEY_LATITUDE = "Latitude";
     public static final String KEY_LONGITUDE = "Longitude";
     public static final String KEY_PLANTS = "plants";
+    public static final String KEY_LOCATION = "location";
+    public static final String KEY_PHOTO = "photo";
+    public static final String KEY_USER = "whoseGarden";
 
     public String getName(){return getString(KEY_NAME);}
 
@@ -33,4 +38,18 @@ public class Garden extends ParseObject {
         currentPlants.add(plant);
         setPlants(currentPlants);
     }
+
+    public String getLocation(){return getString(KEY_LOCATION);}
+
+    public void setLocation(String location){put(KEY_LOCATION, location);}
+
+    public ParseFile getPhoto(){return getParseFile(KEY_PHOTO);}
+
+    public void setPhoto(ParseFile photo){put(KEY_PHOTO, photo);}
+
+    public ParseUser getUser(){return getParseUser(KEY_USER);}
+
+    public void setUser(ParseUser user){put(KEY_USER, user);}
+
+
 }

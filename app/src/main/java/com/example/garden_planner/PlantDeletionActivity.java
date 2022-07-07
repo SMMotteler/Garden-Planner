4118b551b0698eb687edf599e7a7fbbb456d163f
Sarah Motteler <sarahmotteler@fb.com>
2022-07-07 09:47:43 -0700

    when a plantInBed is deleted, any reminders pertaining to the plantInBed object are also deleted

--- app/src/main/java/com/example/garden_planner/PlantDeletionActivity.java
+++ app/src/main/java/com/example/garden_planner/PlantDeletionActivity.java
@@ -10,15 +10,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.garden_planner.adapters.PlantDeletionAdapter;
+import com.example.garden_planner.adapters.ReminderAdapter;
 import com.example.garden_planner.databinding.ActivityPlantDeletionBinding;
 import com.example.garden_planner.models.Garden;
 import com.example.garden_planner.models.PlantInBed;
 import com.example.garden_planner.models.Reminder;
 import com.parse.DeleteCallback;
+import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.annotation.Nullable;
 
 public class PlantDeletionActivity extends AppCompatActivity {
 
@@ -55,23 +61,11 @@ public class PlantDeletionActivity extends AppCompatActivity {
             public void onClick(View view) {
                 List<PlantInBed> plantsToDelete = adapter.getPlantsToDelete();
                 for (PlantInBed plant : plantsToDelete){
-                    List<Reminder> plantReminders = new ArrayList<>();
-                    // get the list of reminders that pertains to deletedPlant
-                    GardenMethodHelper.queryReminders(plantReminders, null, Reminder.KEY_REMIND_WHICH_PLANT, plant, true);
-                    Log.i("reminders", plantReminders.toString());
-                    for (Reminder reminder : plantReminders){
-                        Log.i("deleteReminders", reminder.getReminderTitle()+"is being deleted");
-                        try {
-                            reminder.delete();
-                        } catch (ParseException e) {
-                            e.printStackTrace();
-                        }
-                        Log.i("deleteReminders", "deleted");
-                    }
+                    deleteReminders(plant);
                     plant.deleteInBackground(new DeleteCallback() {
                         @Override
                         public void done(ParseException e) {
-
+                            finish();
                         }
                     });
                 }
@@ -80,4 +74,34 @@ public class PlantDeletionActivity extends AppCompatActivity {
         });
     }
 
+    public void deleteReminders(PlantInBed plant){
+        ParseQuery<Reminder> query = ParseQuery.getQuery(Reminder.class);
+        query.whereEqualTo(Reminder.KEY_REMIND_WHICH_PLANT, plant);
+        query.addAscendingOrder(Reminder.KEY_REMINDER_START);
+        query.include(Reminder.KEY_REMIND_WHICH_PLANT);
+
+        // start an asynchronous call for reminders
+        query.findInBackground(new FindCallback<Reminder>() {
+            @Override
+            public void done(List<Reminder> reminders, ParseException e) {
+                // check for errors
+                if (e != null) {
+                    Log.e("Detail Activity", "Issue with getting gardens", e);
+                    return;
+                }
+
+                // for debugging purposes let's print every reminder title to LogCat, then delete the reminder
+                for (Reminder reminder : reminders) {
+                    Log.i("Reminder Query", "Reminder: " + reminder.getReminderTitle());
+                    try {
+                        reminder.delete();
+                    } catch (ParseException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+
+            }
+        });
+
+    }
 }

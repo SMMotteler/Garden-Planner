01ecbcebfae899685b19d534faab0aedd005bad3
Sarah Motteler <sarahmotteler@fb.com>
2022-07-22 15:52:54 -0700

    fixing issue with create garden toast not appearing

--- app/src/main/java/com/main/garden_planner/CreateGardenActivity.java
+++ app/src/main/java/com/main/garden_planner/CreateGardenActivity.java
@@ -104,120 +104,120 @@ public class CreateGardenActivity extends AppCompatActivity {
                 String gardenLocation = etGardenLocation.getText().toString();
 
                 if(gardenName.isEmpty() || gardenLocation.isEmpty()){
-                    Toast.makeText(CreateGardenActivity.this, "You're missing a field!",
+                    Toast.makeText(getBaseContext(), "You're missing a field!",
                             Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                geocodingClient.forwardGeocoding(gardenLocation, new JsonHttpResponseHandler() {
-                    @Override
-                    public void onSuccess(int statusCode, Headers headers, JSON json) {
-                        try {
-                            latitude = json.jsonObject.getJSONArray("data").getJSONObject(0).getDouble("latitude");
-                            longitude = json.jsonObject.getJSONArray("data").getJSONObject(0).getDouble("longitude");
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                    geocodingClient.forwardGeocoding(gardenLocation, new JsonHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Headers headers, JSON json) {
+                            try {
+                                latitude = json.jsonObject.getJSONArray("data").getJSONObject(0).getDouble("latitude");
+                                longitude = json.jsonObject.getJSONArray("data").getJSONObject(0).getDouble("longitude");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                        throwable.printStackTrace();
-                    }
-                });
+                        @Override
+                        public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                            throwable.printStackTrace();
+                        }
+                    });
 
-                btCreate.setClickable(false);
-                Garden garden = new Garden();
-                garden.setLocation(gardenLocation);
-                garden.setName(gardenName);
-                garden.setUser(ParseUser.getCurrentUser());
-                garden.setLatLong(latitude, longitude);
+                    btCreate.setClickable(false);
+                    Garden garden = new Garden();
+                    garden.setLocation(gardenLocation);
+                    garden.setName(gardenName);
+                    garden.setUser(ParseUser.getCurrentUser());
+                    garden.setLatLong(latitude, longitude);
 
-                frostDateClient.getStations(latitude, longitude, new JsonHttpResponseHandler() {
-                    @Override
-                    public void onSuccess(int statusCode, Headers headers, JSON json) {
-                        // the API for the stations list them in order of increasing distance, so we can always
-                        // take the first JSONObject
-                        try {
-                            JSONObject station = json.jsonArray.getJSONObject(0);
+                    frostDateClient.getStations(latitude, longitude, new JsonHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Headers headers, JSON json) {
+                            // the API for the stations list them in order of increasing distance, so we can always
+                            // take the first JSONObject
+                            try {
+                                JSONObject station = json.jsonArray.getJSONObject(0);
 
-                            frostDateClient.getFrostDate(station.getString("id"), 1, new JsonHttpResponseHandler() {
-                                @Override
-                                public void onSuccess(int statusCode, Headers headers, JSON json) {
-                                    // the API for the frost dates has the 32 deg. threshold as the second entry in
-                                    // the array every time, which we are using as the temperature where no more frost
-                                    // will occur
+                                frostDateClient.getFrostDate(station.getString("id"), 1, new JsonHttpResponseHandler() {
+                                    @Override
+                                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                                        // the API for the frost dates has the 32 deg. threshold as the second entry in
+                                        // the array every time, which we are using as the temperature where no more frost
+                                        // will occur
 
-                                    try {
+                                        try {
 
-                                        Date today = new Date();
-                                        JSONObject lastFrostDateInfo = json.jsonArray.getJSONObject(1);
-                                        String frostDateDayMonth = lastFrostDateInfo.getString("prob_50");
+                                            Date today = new Date();
+                                            JSONObject lastFrostDateInfo = json.jsonArray.getJSONObject(1);
+                                            String frostDateDayMonth = lastFrostDateInfo.getString("prob_50");
 
-                                        // today.getMonth() has January = 0, so we need to add 1 to month to make it match to frostDateDayMonth
-                                        String todayMonth;
-                                        if (today.getMonth() < 9) {
-                                            todayMonth = "0"+(today.getMonth() + 1);
-                                        }
-                                        else{
-                                            todayMonth = ""+(today.getMonth()+1);
-                                        }
-                                        String todayDate;
-                                        if (today.getDate() < 9) {
-                                            todayDate = "0"+(today.getDate());
-                                        }
-                                        else{
-                                            todayDate = ""+(today.getDate());
-                                        }
-                                        String todayDayMonth = todayMonth+todayDate;
-                                        String year;
-                                        if (Integer.valueOf(todayDayMonth) > Integer.valueOf(frostDateDayMonth)){
-                                            year = String.valueOf(today.getYear()+1901);
-                                        }
-                                        else{
-                                            year = String.valueOf(today.getYear()+1900);
-                                        }
-                                        String lastFrostDateDay = lastFrostDateInfo.getString("prob_50")+year;
-
-                                        // return the last frost date
-                                        SimpleDateFormat formatter = new SimpleDateFormat("MMddyyyy");
-                                        frostDate = formatter.parse(lastFrostDateDay);
-                                        garden.setLastFrostDate(frostDate);
-                                        garden.saveInBackground(new SaveCallback() {
-                                            @Override
-                                            public void done(ParseException e) {
-                                                if(e != null){
-                                                    Toast.makeText(CreateGardenActivity.this, "Error in making a garden!!", Toast.LENGTH_SHORT).show();
-                                                    return;
-                                                }
-                                                finish();
+                                            // today.getMonth() has January = 0, so we need to add 1 to month to make it match to frostDateDayMonth
+                                            String todayMonth;
+                                            if (today.getMonth() < 9) {
+                                                todayMonth = "0"+(today.getMonth() + 1);
                                             }
-                                        });
-                                    } catch (JSONException | java.text.ParseException e) {
-                                        e.printStackTrace();
+                                            else{
+                                                todayMonth = ""+(today.getMonth()+1);
+                                            }
+                                            String todayDate;
+                                            if (today.getDate() < 9) {
+                                                todayDate = "0"+(today.getDate());
+                                            }
+                                            else{
+                                                todayDate = ""+(today.getDate());
+                                            }
+                                            String todayDayMonth = todayMonth+todayDate;
+                                            String year;
+                                            if (Integer.valueOf(todayDayMonth) > Integer.valueOf(frostDateDayMonth)){
+                                                year = String.valueOf(today.getYear()+1901);
+                                            }
+                                            else{
+                                                year = String.valueOf(today.getYear()+1900);
+                                            }
+                                            String lastFrostDateDay = lastFrostDateInfo.getString("prob_50")+year;
+
+                                            // return the last frost date
+                                            SimpleDateFormat formatter = new SimpleDateFormat("MMddyyyy");
+                                            frostDate = formatter.parse(lastFrostDateDay);
+                                            garden.setLastFrostDate(frostDate);
+                                            garden.saveInBackground(new SaveCallback() {
+                                                @Override
+                                                public void done(ParseException e) {
+                                                    if(e != null){
+                                                        Toast.makeText(CreateGardenActivity.this, "Error in making a garden!!", Toast.LENGTH_SHORT).show();
+                                                        return;
+                                                    }
+                                                    finish();
+                                                }
+                                            });
+                                        } catch (JSONException | java.text.ParseException e) {
+                                            e.printStackTrace();
+                                        }
                                     }
-                                }
 
-                                @Override
-                                public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                                    throwable.printStackTrace();
-                                }
-                            });
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                                    @Override
+                                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                                        throwable.printStackTrace();
+                                    }
+                                });
+                            } catch (JSONException e) {
+                                e.printStackTrace();
 
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                        throwable.printStackTrace();
-                    }
-                });
+                        @Override
+                        public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                            throwable.printStackTrace();
+                        }
+                    });
 
 
-            }
-        });
+                }
+            });
 
         btFindLocation.setOnClickListener(new View.OnClickListener() {
             @Override
